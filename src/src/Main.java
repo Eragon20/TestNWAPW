2f93487a7a4d7eb70c6450f0564efa1d0a7ca150
Eragon20 <454494@bsd48.org>
2018-05-19 15:03:05 -0700

    fdsxm
    jfd

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
+	
 	public Main() {
 		System.out.println("This is a (very difficult) test.");
 	}
 	public static void main(String[] args) {
 		new Main();
+	}
+	
+	public void read(String[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+			
+		}
 	}
 }
