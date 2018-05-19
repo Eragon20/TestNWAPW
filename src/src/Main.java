a04e643307180182cbc66eadf1b8544196579bea
c1fr1 <c1fr1.stkcps@gmail.com>
2018-05-19 23:18:28 +0100

    reads the test

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 	public Main() {
-		Scanner s = new Scanner("Test");
+		Scanner s = new Scanner(System.in);
+		try {
+			s = new Scanner(new File("C:/Users/Varas/IdeaProjects/TestNWAPW/src/Test.txt"));
+		}catch (IOException e) {
+			System.out.println(e);
+		}
 		System.out.println(s.next());
 		System.out.println("This is a (very difficult) test.");
 	}
